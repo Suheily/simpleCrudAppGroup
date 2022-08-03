4b45bcdecda48164a3b831afe0d8568737afebc1
meghann <meghannfh@gmail.com>
2022-08-03 21:27:07 +0900

    added to server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3200;
 
 let db;
 let dbConnectionString = process.env.DB_CONNECTION;
-let dbName = 'albums';
+let dbName = 'album-list';
 
 
 MongoClient.connect(dbConnectionString, { useUnifiedTopology: true})
@@ -21,6 +21,40 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.listen(PORT, ()=>{
+// app.get("/", async(req, res) =>{
+//     try {
+//         const albumsNames = db.collection('albumnames').find().toArray();
+//     res.render("index.ejs", {
+//         albums: albumsNames,
+//         artists: artistNames,
+//     })
+//     } catch (error) {
+//         error => console.log(error)
+//     }
+// })
+
+
+app.get('/', (req, res)=>{
+    db.collection('albums').find().toArray()
+    .then(data => {
+        res.render('index.ejs', { info: data })
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/addAlbum', (req, res)=> {
+    db.collection('albums').insertOne({ 
+        albumname: req.body.albumname, 
+        artistname: req.body.artistname,
+        phot: req.body.photo,
+        likes: 0
+    })
+    .then(result => {
+        res.redirect('/')
+    })
+})
+
+
+.listen(PORT, ()=>{
     console.log('The server is running on PORT ' + PORT)
 });
